39263e411ea5d58c42937e394215ed469571e9f0
Simran850985-03-43539805 <162444827+Simran850985-03-43539805@users.noreply.github.com>
2024-05-23 18:06:44 +0530

    Update scriptsi.js

--- scriptsi.js
+++ scriptsi.js
@@ -1,30 +1,7 @@
-document.getElementById("colorButton").addEventListener("click", changeColor);
-
-function changeColor() {
-    var button = document.getElementById("colorButton");
-    var currentColor = button.style.backgroundColor;
-    button.style.backgroundColor = currentColor === "red" ? "white" : "red";
+function addNumbers() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 + num2;
+    document.getElementById('result').innerText = 'Result: ' + result;
 }
 
-function displayGreeting() {
-    var today = new Date();
-    var hour = today.getHours();
-
-    var greeting;
-    if (hour < 12) {
-        greeting = "Hii I am Simran!";
-    } else if (hour < 18) {
-        greeting = "I am From Bihar!";
-    } else {
-        greeting = "I am Doing some work!";
-    }
-
-    alert(greeting);
-}
-
-function SubtractNumbers() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var result = num1 - num2;
-    document.getElementById("result").innerHTML = "Result: " + result;
-}
